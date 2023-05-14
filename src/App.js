cf50ae52298b982608d5e471430f63f07e68b128
kmlaird <kmlaird15@gmail.com>
2023-05-14 11:33:46 -0700

    Footer added and unnecessary css removed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
       <header className="App-header">
         <h1>Weather Application</h1>
       </header>
+      <main></main>
+      <footer>
+        <a
+          href="https://github.com/kmlaird/weather-app-react.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/kimlaird/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kim Laird
+        </a>
+      </footer>
     </div>
   );
 }
