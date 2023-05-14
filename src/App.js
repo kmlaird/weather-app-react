df93676c351d8f1f511f49d5d3a531a91945daf8
kmlaird <kmlaird15@gmail.com>
2023-05-14 11:44:39 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Application</h1>
-      </header>
-      <main></main>
-      <footer>
-        <a
-          href="https://github.com/kmlaird/weather-app-react.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced code
-        </a>{" "}
-        by{" "}
-        <a
-          href="https://www.linkedin.com/in/kimlaird/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kim Laird
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather Application</h1>
+        </header>
+        <main></main>
+        <footer>
+          <a
+            href="https://github.com/kmlaird/weather-app-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/kimlaird/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kim Laird
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
